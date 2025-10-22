2040bab475360a3330deaab4f8b1fdcb208668e6
Ayaindeed <aya.mechouahiriffi@usmba.ac.ma>
2025-10-22 22:19:14 +0100

    fixed the layout v2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Lá Breithe Sona Duit',
+  title: 'Grá <3',
   description: 'A special birthday celebration',
 }
 
